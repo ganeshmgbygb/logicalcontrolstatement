d3e5eb3766c84a09f317db4eee5ab26b62eba3ed
ganeshmgbygb <118801934+ganeshmgbygb@users.noreply.github.com>
2023-10-03 20:09:47 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+public class age{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the age");
+    int age=sc.nextInt();
+    if(age<=18){
+      System.out.println("You are a minor");
+    }
+    if(age<=65){
+      System.out.println("You are adult");
+    }
+    if(age>=65){
+      System.out.println("You are a senior citizen");
+    }
+  }
+}
